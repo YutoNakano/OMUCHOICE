fec8c194f04cab4bc8a98787384555f84d77cedf
Yuto Nakano <yuto.44.bb@gmail.com>
2019-01-25 18:13:41 +0900

    add new file

---
+++ config.py
@@ -0,0 +1,25 @@
+# sorry
+# twitterAPI
+CONSUMER_KEY = "IrVPhFuB6YTOvlyUpmWiLqWVD"
+CONSUMER_SECRET = "KDUFOC5ksvEWpFmdsPFgF750eoB0vBEkOhNXwO7QWgpFeY1tuk"
+ACCESS_TOKEN = "982945093966417921-2fT1ZDkHVVZkMlc5Uduxal0VWVwpuOI"
+ACCESS_TOKEN_SECRET = "UDHaQ5mawfDkyoKwMAOGLHI1UDsowVYp7CPtrRGp6HgUX"
+
+# ぐるなびAPI
+ACCESS_KEY = "925a86e315331cd2d644a6f8fe2d2df8"
+
+
+def getConsumerKey():
+    return CONSUMER_KEY
+
+
+def getConsumerSecret():
+    return CONSUMER_SECRET
+
+
+def getAccessToken():
+    return ACCESS_TOKEN
+
+
+def getAccessSecret():
+    return ACCESS_TOKEN_SECRET
