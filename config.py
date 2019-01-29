a94a3c84f5e67e91e07d9ddb367f5b6608935871
Yuto Nakano <yuto.44.bb@gmail.com>
2019-01-29 20:03:56 +0900

    fix config.py

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 import os
-
 from dotenv import load_dotenv
+
+
 
 print(load_dotenv())
 
-CONSUMER_KEY = os.getenv("CONSUMER_KEY")
-CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
-ACCESS_KEY=os.getenv("ACCESS_KEY")
+CONSUMER_KEY = os.environ("CONSUMER_KEY")
+CONSUMER_SECRET = os.environ("CONSUMER_SECRET")
+ACCESS_TOKEN = os.environ("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.environ("ACCESS_TOKEN_SECRET")
+ACCESS_KEY=os.environ("ACCESS_KEY")
 
  
 
