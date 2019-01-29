609fc439262e721ab4372ae6e01569c4bb74ca7d
Yuto Nakano <yuto.44.bb@gmail.com>
2019-01-29 20:21:32 +0900

    fix config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 from dotenv import load_dotenv
-
+from boto.s3.connection import S3Connection
 
 
 print(load_dotenv())
 
-CONSUMER_KEY = os.environ("CONSUMER_KEY")
-CONSUMER_SECRET = os.environ("CONSUMER_SECRET")
-ACCESS_TOKEN = os.environ("ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET = os.environ("ACCESS_TOKEN_SECRET")
-ACCESS_KEY=os.environ("ACCESS_KEY")
+CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+ACCESS_KEY=os.getenv("ACCESS_KEY")
 
- 
+S3 = S3Connection(os.environ['CONSUMER_KEY'],os.environ['CONSUMER_SECRET'],os.environ['ACCESS_TOKEN'],os.environ['ACCESS_TOKEN_SECRET'],os.environ['ACCESS_KEY'])
 
 
 def getConsumerKey():
